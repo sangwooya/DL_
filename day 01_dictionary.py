ca0e1a3eaadeeb72b279e701c663f179c87dcc5f
bangsangwoo <sangwoo6999@naver.com>
2023-01-12 18:07:24 +0900

    first commit

---
+++ day 01_dictionary.py
@@ -0,0 +1,7 @@
+subjects = {'의사소통영어':'a',
+            '오래된 미래': 'b',
+            '양자역학':'c'}
+student = '김도훈'
+subject = '오래된 미래'
+print(f"{student}, 학생의 {subject} '과목의 성적은' , {subjects[subject]} 입니다.")
+print("{}학생의 {} 과목의 성적은 {} 입니다.".format(student,subject,subjects[subject]))
---
+++ day 01_gul.py
@@ -0,0 +1,6 @@
+import tkinter as tk
+
+win = tk.Tk()
+win.geometry('1690x900')
+win.title('파이썬 1일차')
+win.mainloop()
