956d4d1cc798944e90ea329c4555d1c0ae414bd8
bangsangwoo <sangwoo6999@naver.com>
2023-01-12 18:22:51 +0900

    fourth

--- day 01.py
+++ day 01.py
@@ -1,7 +1,3 @@
-subjects = {'의사소통영어':'a',
-            '오래된 미래': 'b',
-            '양자역학':'c'}
-student = '김도훈'
-subject = '오래된 미래'
-print(f"{student}, 학생의 {subject} '과목의 성적은' , {subjects[subject]} 입니다.")
-print("{}학생의 {} 과목의 성적은 {} 입니다.".format(student,subje
+a= float(input("섭씨 온도는?"))
+b= 9/5*a +32
+print("화씨온도는=",b)
