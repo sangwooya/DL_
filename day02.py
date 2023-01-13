f6fe87bb92b70c814f6822c412f3ad273efacf5d
bangsangwoo <sangwoo6999@naver.com>
2023-01-13 14:00:55 +0900

    6

--- day02.py
+++ day02.py
@@ -1,1 +1,19 @@
-re
+test = 1,2
+print(type(test))  #packing
+print(type(test))
+print(test)
+print(test[1]) #unpacking
+print(3+4)
+print(0o16)                    #ob=2진수 0o=8진수 0x=16진수
+number=154
+print(bin(number))          #2진수
+print(hex(number))          #16진수
+print(oct(number))          #8진수
+print(ord("u"))             #단일 문자열(아스키코드를)을 10진수로 변경
+print(chr(100))             #숫자를 아스키코드로 변경, 16진수도 되나?
+print(chr(0x61))            #되네
+print(0x61)
+
+
+
+
