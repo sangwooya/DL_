7766c49df87fa98a934e2a77bcfcd0be7722ad0a
bangsangwoo <sangwoo6999@naver.com>
2023-01-13 13:11:01 +0900

    5

--- day 01_countdown.py
+++
@@ -1,3 +0,0 @@
-for countdown in 5, 4, 3, 2, 1:
-    print(countdown)
-    print(3)
--- day02.py
+++ day02.py
@@ -1,3 +1,1 @@
-math_values = [3.14,2.71]
-print(f'원주율의 값은{math_values[0]}이고 타입은 {type(math_values)}입니다. ')
-math_values[0]=9.99
+re
